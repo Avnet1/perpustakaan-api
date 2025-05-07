183f8401b6f4ee6891716f6ade006079ff7e9642
alpredoruben <alpredo.tampubolon@gmail.com>
2025-05-07 09:48:21 +0700

    fix organisasi

--- app/Http/Requests/OrganizationRequest.php
+++ app/Http/Requests/OrganizationRequest.php
@@ -59,12 +59,13 @@ class OrganizationRequest extends FormRequest
                     'domain_admin_url' => 'required',
                     'domain_website_url' => 'required',
                     'password' => 'required',
+                    'status' => 'required',
                 ];
 
             case config('constants.route_name.superadmin.organization.assign-module'):
                 return [
                     'list_modules' => 'required',
-                    'status' => 'required',
+
                 ];
 
 
