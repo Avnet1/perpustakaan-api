34fd2cf452d09011ae758479687f18a6255b0788
alpredoruben <alpredo.tampubolon@gmail.com>
2025-05-09 11:42:08 +0700

    add clients migrations

---
+++ database/migrations/clients/2025_05_09_043753_create_client_config.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_config', function (Blueprint $table) {
+            $table->uuid('config_id')->primary();
+            $table->string('code_member')->nullable()->default(null);
+            $table->string('organization_name')->nullable()->default(null);
+            $table->text('logo')->nullable()->default(null);
+            $table->string('email')->nullable()->default(null);
+            $table->string('password')->nullable()->default(null);
+            $table->boolean('is_approved')->nullable()->default(false);
+            $table->timestamp('approved_at')->nullable()->default(null);
+            $table->text('domain_admin_url')->nullable()->default(null);
+            $table->text('domain_website_url')->nullable()->default(null);
+            $table->boolean('status')->nullable()->default(false);
+            $table->string('db_user')->nullable()->default(null);
+            $table->string('db_pass')->nullable()->default(null);
+            $table->string('db_name')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes(); // deleted_at
+            $table->uuid('created_by')->nullable();
+            $table->uuid('updated_by')->nullable();
+            $table->uuid('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_config');
+    }
+};
